a5a0652128a301369662de9b0a59b0fa8cfc7914
selly361 <seikou615@gmail.com>
2023-12-28 20:06:38 +0000

    Refactored app with markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { useState } from 'react'
+import faqData from 'Data/data.json'
+import { StarIcon } from 'Icons'
 
 function App() {
-  return <main></main>
+  const [data, setData] = useState(faqData)
+
+  const setActive = (id: number) => setData((e) => ({ ...e, active: data.active == id ? -1 : id }))
+
+  return (
+    <main className='main'>
+      <section className='main__top-section'>
+        <StarIcon />
+        <h2>FAQs</h2>
+      </section>
+
+      <div className='main__faq'>
+
+        {data.faqs.map((faq) => (
+          <div key={faq.id} className='main__faq__dropdown'>
+            <div className='main__faq__dropdown__wrapper'>
+              <h5 className='main__faq__dropdown__wrapper_question'>{faq.question}</h5>
+              <button
+                onClick={() => setActive(faq.id)}
+                className={`main__faq__dropdown__wrapper_button ${faq.id == data.active ? 'main__faq__dropdown__wrapper_button--active' : ''}`}/>
+            </div>
+
+            {faq.id == data.active ? (
+              <p className='main__faq__dropdown_answer'>{faq.answer}</p>
+            ) : null}
+
+          </div>
+        ))}
+
+      </div>
+    </main>
+  )
 }
 
 export default App
